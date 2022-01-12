e3830cad6cd5818ed8326ece3ad81b96bce3784c
MaRCode1 <matteo.remorini@gmail.com>
2022-01-12 21:48:50 +0100

    Generated components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MapComponent } from './map/map.component';
+import { CardComponent } from './card/card.component';
+import { SidePanelComponent } from './side-panel/side-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MapComponent,
+    CardComponent,
+    SidePanelComponent
   ],
   imports: [
     BrowserModule
